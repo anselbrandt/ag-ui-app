30cbbff61d02aa06e131bad7337c3983e24872f1
anselbrandt <ansel@anselbrandt.com>
2026-01-01 17:17:01 -0800

    Add logfire

--- agent/app/agent.py
+++ agent/app/agent.py
@@ -1,31 +1,39 @@
 from textwrap import dedent
+from ag_ui.core import EventType, StateSnapshotEvent
 from pydantic import BaseModel, Field
 from pydantic_ai import Agent, RunContext
 from pydantic_ai.ag_ui import StateDeps
-from ag_ui.core import EventType, StateSnapshotEvent
 from pydantic_ai.models.openai import OpenAIResponsesModel
+import logfire
 
 # load environment variables
 from dotenv import load_dotenv
+
 load_dotenv()
+
+logfire.configure(send_to_logfire="if-token-present")
+logfire.instrument_pydantic_ai()
+
 
 # =====
 # State
 # =====
 class ProverbsState(BaseModel):
-  """List of the proverbs being written."""
-  proverbs: list[str] = Field(
-    default_factory=list,
-    description='The list of already written proverbs',
-  )
+    """List of the proverbs being written."""
+
+    proverbs: list[str] = Field(
+        default_factory=list,
+        description="The list of already written proverbs",
+    )
+
 
 # =====
 # Agent
 # =====
 agent = Agent(
-  model = OpenAIResponsesModel('gpt-4.1-mini'),
-  deps_type=StateDeps[ProverbsState],
-  system_prompt=dedent("""
+    model=OpenAIResponsesModel("gpt-4.1-mini"),
+    deps_type=StateDeps[ProverbsState],
+    system_prompt=dedent("""
     You are a helpful assistant that helps manage and discuss proverbs.
     
     The user has a list of proverbs that you can help them manage.
@@ -33,36 +41,43 @@ agent = Agent(
     
     When discussing proverbs, ALWAYS use the get_proverbs tool to see the current list before
     mentioning, updating, or discussing proverbs with the user.
-  """).strip()
+  """).strip(),
 )
+
 
 # =====
 # Tools
 # =====
 @agent.tool
 def get_proverbs(ctx: RunContext[StateDeps[ProverbsState]]) -> list[str]:
-  """Get the current list of proverbs."""
-  print(f"📖 Getting proverbs: {ctx.deps.state.proverbs}")
-  return ctx.deps.state.proverbs
+    """Get the current list of proverbs."""
+    print(f"📖 Getting proverbs: {ctx.deps.state.proverbs}")
+    return ctx.deps.state.proverbs
+
 
 @agent.tool
-async def add_proverbs(ctx: RunContext[StateDeps[ProverbsState]], proverbs: list[str]) -> StateSnapshotEvent:
-  ctx.deps.state.proverbs.extend(proverbs)
-  return StateSnapshotEvent(
-    type=EventType.STATE_SNAPSHOT,
-    snapshot=ctx.deps.state,
-  )
+async def add_proverbs(
+    ctx: RunContext[StateDeps[ProverbsState]], proverbs: list[str]
+) -> StateSnapshotEvent:
+    ctx.deps.state.proverbs.extend(proverbs)
+    return StateSnapshotEvent(
+        type=EventType.STATE_SNAPSHOT,
+        snapshot=ctx.deps.state,
+    )
+
 
 @agent.tool
-async def set_proverbs(ctx: RunContext[StateDeps[ProverbsState]], proverbs: list[str]) -> StateSnapshotEvent:
-  ctx.deps.state.proverbs = proverbs
-  return StateSnapshotEvent(
-    type=EventType.STATE_SNAPSHOT,
-    snapshot=ctx.deps.state,
-  )
+async def set_proverbs(
+    ctx: RunContext[StateDeps[ProverbsState]], proverbs: list[str]
+) -> StateSnapshotEvent:
+    ctx.deps.state.proverbs = proverbs
+    return StateSnapshotEvent(
+        type=EventType.STATE_SNAPSHOT,
+        snapshot=ctx.deps.state,
+    )
 
 
 @agent.tool
 def get_weather(_: RunContext[StateDeps[ProverbsState]], location: str) -> str:
-  """Get the weather for a given location. Ensure location is fully spelled out."""
-  return f"The weather in {location} is sunny."
+    """Get the weather for a given location. Ensure location is fully spelled out."""
+    return f"The weather in {location} is sunny."
